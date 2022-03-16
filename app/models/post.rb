aeb12e4a5fe12ebf54e0be470cdbc979e10c4948
Matt Draghi <mtd5264@gmail.com>
2022-03-16 16:44:26 -0400

    commit tree e1c90151ea2e7a9b687003e1c1204548f063ea04

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Non-Fiction Fiction)}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        # unless title.match?(/Won't Believe|Secret|Top \d+|Guess/)
+        #     title.errors
+        # end
+    end
 end
